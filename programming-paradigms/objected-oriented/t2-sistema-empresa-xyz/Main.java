2fef84fb2e61046b46aceb49bd10cbc95cf4408e
Bárbara Este <barbaraeste01@gmail.com>
2020-06-13 23:42:49 -0300

    initial implementation of Empresa methods

--- programming-paradigms/objected-oriented/t2-sistema-empresa-xyz/Main.java
+++ programming-paradigms/objected-oriented/t2-sistema-empresa-xyz/Main.java
@@ -2,10 +2,10 @@ import classes.*;
 
 class Main {
     public static void main(String[] args) {
-        BaseColaborador estag = new Estagiario("Barbara", 1, 1000, 4.50);
-        BaseColaborador presidente = new Presidente("Mulher mainframe", 0, 32.600);
-        ((Estagiario) estag).setValeCoxinha(3.5);
-        System.out.println(estag);
-        System.out.println(presidente);
+        Empresa empresa = new Empresa();
+
+        empresa.relatorio();
+        empresa.aumentaAdicional(2.1);
+        empresa.relatorio();
     }
 }
--- programming-paradigms/objected-oriented/t2-sistema-empresa-xyz/classes/Empresa.java
+++ programming-paradigms/objected-oriented/t2-sistema-empresa-xyz/classes/Empresa.java
@@ -1,9 +1,35 @@
 package classes;
 
 public class Empresa {
+    private int quantidadeFuncionarios = 2;
     private BaseColaborador[] funcionarios;
 
-    public Empresa(int qte_max_funcionarios) {
-        this.funcionarios = new BaseColaborador[qte_max_funcionarios];
+    public Empresa() {
+        this.funcionarios = new BaseColaborador[100];
+        this.funcionarios[0] = new Estagiario("Barbara", 1, 1000, 4.50);
+        this.funcionarios[1] = new Presidente("Mulher mainframe", 0, 32.600);
+    }
+
+    /**
+     * Aumenta o adicional dos funcionarios da empresa.
+     * @param novo_percentual
+     */
+    public void aumentaAdicional(double novo_percentual) {
+        int i;
+        for (i = 0; i < this.quantidadeFuncionarios; i++) {
+            if (this.funcionarios[i] instanceof Funcionario) {
+                ((Funcionario) this.funcionarios[i]).setAdicional(novo_percentual);
+            }
+        }
+    }
+
+    /**
+     * Relatorio de todos os colaboradores da Empresa
+     */
+    public void relatorio() {
+        int i;
+        for (i = 0; i < this.quantidadeFuncionarios; i++) {
+            System.out.println(this.funcionarios[i]);
+        }
     }
 }
