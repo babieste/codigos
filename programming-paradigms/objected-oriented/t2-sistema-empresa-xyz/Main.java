c285dfe459a4b97737d6c3235b9d1caae71bd825
Bárbara Este <barbaraeste01@gmail.com>
2020-06-14 17:47:28 -0300

    updates Main.java

--- programming-paradigms/objected-oriented/t2-sistema-empresa-xyz/Main.java
+++ programming-paradigms/objected-oriented/t2-sistema-empresa-xyz/Main.java
@@ -23,8 +23,9 @@ class Main {
 
         switch (menuChoice) {
             case 1: // Adicionar Colaborador
-                // menuAdicionarColaborador();
+                menuAdicionarColaborador(s);
                 break;
+
             case 2: // Calcular pagamento dos Funcionarios
                 break;
 
@@ -32,10 +33,64 @@ class Main {
                 break;
 
             case 4: // Relatorio
+                break;
+
+            case 5: // Sair do programa
             default:
                 break;
         }
 
         s.close();
+    }
+
+    public static void menuAdicionarColaborador(Scanner s) {
+        int menuChoice = 0;
+
+        do {
+            System.out.println("Qual o tipo de colaborador?");
+            System.out.println("1 - Estagiario");
+            System.out.println("2 - Funcionario");
+            System.out.println("3 - Presidente ");
+            System.out.print("Escolha: ");
+
+            if (s.hasNextInt()) menuChoice = s.nextInt();
+            else menuChoice = 0;
+        } while ((menuChoice < 1) || (menuChoice > 3));
+
+        switch (menuChoice) {
+            case 1: // Adicionar Estagiario
+                menuAdicionarEstagiario(s);
+                break;
+
+            case 2: // Adicionar Funcionario
+                break;
+
+            case 3: // Adicionar Presidente
+            default:
+                break;
+        }
+    }
+
+    public static void menuAdicionarEstagiario(Scanner s) {
+        String nome;
+        int departamento;
+        double salario, valecoxinha;
+
+        if(s.hasNext()) s.nextLine();
+
+        System.out.println("--- Adicionar Estagiario ---");
+        System.out.print("Nome: ");
+        nome = s.nextLine();
+
+        System.out.print("Departamento: ");
+        departamento = s.nextInt();
+
+        System.out.print("Salario: ");
+        salario = s.nextDouble();
+
+        System.out.print("Vale coxinha: ");
+        valecoxinha = s.nextDouble();
+
+        System.out.println("Estagiario: " + nome + ", departamento: " + departamento + ", salario: " + salario + ", vale coxinha: " + valecoxinha);
     }
 }
